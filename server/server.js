96d32d649333b7bde722b774ad0cc3f4a7c8dcf1
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2018-07-07 23:14:51 +0530

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -29,6 +30,26 @@ app.get('/todos', (req, res) => {
     .catch(e => {
       res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  } else {
+    Todo.findById(id)
+      .then(todo => {
+        if (!todo) {
+          res.status(404).send('Todo not found');
+        } else {
+          res.send({ todo });
+        }
+      })
+      .catch(e => {
+        res.status(400).send(e);
+      });
+  }
 });
 
 app.listen(3000, () => {
